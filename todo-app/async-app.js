bcc8be20dc40f70fa6dfc84599b7f0b70b70a67a
Anil Joseph <anil.jos@gmail.com>
2020-08-04 17:09:54 +0530

    Day 1 over

---
+++ todo-app/async-app.js
@@ -0,0 +1,39 @@
+
+const chalk = require('chalk')
+const yargs = require('yargs')
+const tasks = require("./async-task");
+
+console.log(chalk.blue("TODO Application"));
+
+//node app.js add --taskName CallOffice --description "Call Boss"
+//node app.js list
+//node app.js delete --taskName CallOffice
+
+yargs
+    .command("add", "To add a task", (yargs) => {
+        yargs
+            .option("taskName", {alias: "t", demand: true})
+            .option("desc", {alias: "d", demand: true})
+    }, (args) => {
+        console.log(chalk.blackBright("Add a task"));
+        const taskName = args.taskName;
+        const desc = args.desc;
+        tasks.saveTask(taskName, desc, () => {
+            console.log(chalk.blue.inverse("Saved"))
+        }, (err) => {
+            console.log(chalk.red.inverse("Error in save", err));
+        });
+    })
+    .command("list", "To List all tasks", (yargs) => {}, (args) => {
+
+        console.log(chalk.blackBright("List all tasks"));
+        var promise = tasks.fetchAllTasks();
+        promise.then((data) => {
+            console.log("List", data);
+        }, (err) => {
+            console.log("error")
+        })
+
+    })
+    .command("delete", "To delete a task", (yargs) => {}, (args) => {})
+    .argv;
---
+++ todo-app/async-task.js
@@ -0,0 +1,54 @@
+
+
+const fs = require('fs');
+const { resolve } = require('path');
+const fileName = "tasks.json";
+
+function saveTask(taskName, desc, successCb, errorCb){
+
+    let allTasks = [];
+    fs.readFile(fileName, (err, data) => {
+
+        if(err){
+            
+        }
+        else{
+            allTasks = JSON.parse(data);
+        }
+        allTasks.push({taskName, desc});
+        fs.writeFile(fileName, JSON.stringify(allTasks), (err) => {
+
+            if(err){
+                errorCb(err);
+                return;
+            }
+
+            successCb();
+        });
+    });
+    //return "";
+
+}
+
+function fetchAllTasks(){
+
+    return new Promise( (resolve, reject) => {
+
+        fs.readFile(fileName, (err, data)=> {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(JSON.parse(data));
+            }
+        });
+    });
+}
+
+function deleteTask(taskName){
+
+}
+
+module.exports =  {
+    saveTask, fetchAllTasks, deleteTask
+};
